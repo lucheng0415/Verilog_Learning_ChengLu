44206522ab39cbbc2d35c55ec050e5ea44939edc
CHENG LU <jason.cheng.lu@hotmail.com>
2026-07-20 08:26:52 +1200

    update my_driver design to modern parameters list syntax
    Signed-off-by: CHENG LU <jason.cheng.lu@hotmail.com>

--- Verilog/UVMPractise/my_driver_design.sv
+++ Verilog/UVMPractise/my_driver_design.sv
@@ -1,18 +1,9 @@
-module dut(clk,
-           rst_n,
-           rxd,
-           rx_dv,
-           txd,
-           tx_en);
- input clk;
- input rst_n;
- input[7:0] rxd;
- input rx_dv;
- output [7:0] txd;
- output tx_en;
-
- reg[7:0] txd;
- reg tx_en;
+module dut(input            clk,
+           input            rst_n,
+           input      [7:0] rxd,
+           input            rx_dv,
+           output reg [7:0] txd,
+           output reg       tx_en);
 
  always @(posedge clk) begin
     if(!rst_n) begin
